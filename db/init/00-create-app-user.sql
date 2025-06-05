e0f0674955aae7ed04552aa999f402bbc679b522
v.rabecky <v.rabetskii@gmail.com>
2025-06-05 13:13:32 +0700

    Удалили комменты

--- db/init/00-create-app-user.sql
+++ db/init/00-create-app-user.sql
@@ -22,23 +22,20 @@ END IF;
 END
 $$;
 
--- Назначаем владельца на всякий случай (если БД уже существовала)
 ALTER DATABASE msdb OWNER TO app_user;
 
 -- Выдаём все права на базу данных
 GRANT ALL PRIVILEGES ON DATABASE msdb TO app_user;
 
--- Подключаемся к msdb перед дальнейшими GRANT-ами
 \c msdb
 
--- Гарантируем доступ к схеме public
 GRANT USAGE, CREATE ON SCHEMA public TO app_user;
 
--- Таблицы и последовательности, уже существующие:
+
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO app_user;
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO app_user;
 
--- Таблицы и последовательности, которые будут создаваться в будущем:
+
 ALTER DEFAULT PRIVILEGES IN SCHEMA public
   GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO app_user;
 
